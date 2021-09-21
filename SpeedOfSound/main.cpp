cc1351d3336037e64b4e1e2856c858f879dd3e50
ryan2135 <ryan2135@pacificu.edu>
2021-09-20 18:54:39 -0700

    Add project files.

---
+++ SpeedOfSound/main.cpp
@@ -0,0 +1,19 @@
+//**********************************************************************
+// File:				main.cpp
+// Author:			
+// Date:				9/21/21
+// Class:				CS150-XX
+// Assignment:	Speed of Sound lab
+// Purpose:			
+// Hours:				
+//**********************************************************************
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main () {
+
+	return EXIT_SUCCESS;
+}
